3aced6096ef69b2d65e106c45f81054384f105de
vitaliyboh <vitaliyb@students.zcu.cz>
2022-10-13 18:15:21 +0200

    vytvoreni sveta bez cest

--- Svet.java
+++ Svet.java
@@ -1,3 +1,15 @@
+import java.util.ArrayList;
+
 public class Svet {
-    
+    Oaza[] oazy;
+    Sklad[] sklady;
+    DruhVelblouda[] druhyVelbloudu;
+    Pozadavek[] pozadavky;
+
+    public Svet(Oaza[] oazy, Sklad[] sklady, DruhVelblouda[] druhyVelbloudu, Pozadavek[] pozadavky) {
+        this.oazy = oazy;
+        this.sklady = sklady;
+        this.druhyVelbloudu = druhyVelbloudu;
+        this.pozadavky = pozadavky;
+    }
 }
