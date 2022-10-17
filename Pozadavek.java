9fe6e4f6c3e884d2af726381b06b4c8f7a83943f
mdobrovsky <mdobrovsky88@gmail.com>
2022-10-17 19:10:15 +0200

    oprava pozice a pridani dat

--- Pozadavek.java
+++ Pozadavek.java
@@ -1,8 +1,8 @@
 public class Pozadavek {
-    private int tz;
-    private int op;
-    private int kp;
-    private int tp;
+    private int tz; // cas prichodu pozadavku
+    private int op; // index oazy do ktere ma byt dorucen
+    private int kp; // mnozstvi kosu ktere oaza potrebuje
+    private int tp; // udávající, za jak dlouho po příchodu požadavku musí být koše doručeny
 
     public Pozadavek(int tz, int op, int kp, int tp) {
         this.tz = tz;
