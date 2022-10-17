6e0b70d7f318b9e77ba1bb9fcb2c38514ca93226
vitaliyboh <vitaliyb@students.zcu.cz>
2022-10-17 20:42:19 +0200

    generovani rychlosti a dalsich atributu velbloudu

--- DruhVelblouda.java
+++ DruhVelblouda.java
@@ -7,6 +7,7 @@ public class DruhVelblouda {
     private final double td;
     private final double kd;
     private final double pd;
+    public int pocet;
 
     public DruhVelblouda(String jmeno, double v_min, double v_max,
                          double d_min, double d_max, double td,
@@ -19,5 +20,42 @@ public class DruhVelblouda {
         this.td = td;
         this.kd = kd;
         this.pd = pd;
+        this.pocet = 0;
+    }
+
+    public String getJmeno() {
+        return jmeno;
+    }
+
+    public double getV_min() {
+        return v_min;
+    }
+
+    public double getV_max() {
+        return v_max;
+    }
+
+    public double getD_min() {
+        return d_min;
+    }
+
+    public double getD_max() {
+        return d_max;
+    }
+
+    public double getTd() {
+        return td;
+    }
+
+    public double getKd() {
+        return kd;
+    }
+
+    public double getPd() {
+        return pd;
+    }
+
+    public int getPocet() {
+        return pocet;
     }
 }
--- Velbloud.java
+++ Velbloud.java
@@ -1,9 +1,34 @@
+import java.util.Random;
+
 public class Velbloud {
-    private String druh;
+    private String jmeno;
+    private DruhVelblouda druh; // druh velblouda
     private double v; // rychlost
     private double d; // vzdalenost na jedno napiti
-    private double td;
-    private int kd;
+    private double td; // doba napiti
+    private int kd; // maximalni pocet kosu, ktere bloud unese
+    Random r = new Random();
 
+    public Velbloud(DruhVelblouda druh, double v, double d, double td, int kd) {
+        this.druh = druh;
+        this.v = generateV();
+        this.d = generateD();
+        this.td = td;
+        this.kd = kd;
+        druh.pocet++;
+        this.jmeno = generateName();
+    }
+
+    public double generateV() {
+        return r.nextDouble(druh.getV_max() + Double.MIN_VALUE - druh.getV_min()) + druh.getV_min();
+    }
+
+    public double generateD() {
+        return r.nextGaussian((druh.getD_min() + druh.getD_max())/2.0,(druh.getD_max() - druh.getD_min())/4.0);
+    }
+
+    public String generateName() {
+        return "Velboud_" + druh.pocet;
+    }
 
 }
