0682abe51093fda6feeeb223d8ddfa4526677dbd
vitaliyb <vitalijbogera@gmail.com>
2022-11-30 20:05:21 +0100

    mkmmm

--- Graph.java
+++ Graph.java
@@ -13,8 +13,6 @@ public class Graph {
         this.sklady = sklady;
         this.oazy = oazy;
         edges = new Link[delka];
-
-
     }
 
     public void addEdge(int i, int j) {
@@ -35,6 +33,40 @@ public class Graph {
         edges[j] = link;
         link = new Link(j, edges[i], value);
         edges[i] = link;
+    }
+
+    double[] Dijkstra(int s) {
+        int[] mark = new int[edges.length]; // pocet vrcholu!
+        mark[s] = 1;
+        double[] result = new double[edges.length];
+        for (int i = 0; i < result.length; i++)
+            result[i] = Double.POSITIVE_INFINITY;
+        result[s] = 0;
+        PriorityQueue q = new PriorityQueue();
+        q.add(s, 0);
+        while(q.getMin()>=0) {
+            int v = q.getMin();
+            q.removeMin();
+            Link nbLink = edges[v];
+            while(nbLink!=null) {
+                int n = nbLink.neighbour;
+                if (mark[n] != 2) {
+                    double newDistance = result[v] + nbLink.edgeValue;
+                    if (mark[n] == 0) {
+                        mark[n] = 1;
+                        result[n] = newDistance;
+                        q.add(n, newDistance);
+                    }
+                    else if (newDistance<result[n]) {
+                        result[n] = newDistance;
+                        q.update(n, newDistance);
+                    }
+                }
+                nbLink = nbLink.next;
+            }
+            mark[v] = 2;
+        }
+        return result;
     }
 
 }
